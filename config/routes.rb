da92d7c6e778b9c923dbd9ac10a8d268e8d86d62
GERARDO CASTRO <gicastro@uc.cl>
2022-08-02 23:59:13 -0400

    funcionando Inicio controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
+
+  # root "inicio#index"
+  get "/inicio", to: "inicio#index"
+
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
