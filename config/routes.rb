a66343e87a0f082e36c7785691fbaa27b9034919
GERARDO CASTRO <gicastro@uc.cl>
2022-08-07 02:27:48 -0400

    se cambia el controlador Inicio por Home

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,11 @@
 Rails.application.routes.draw do
 
-  # root "inicio#index"
-  get "/inicio", to: "inicio#index"
+  root "home#index"
+
+  # ==========================================
+  # get "peticion", to: "controlador#metodo"
+  # ==========================================
+  get "/inicio", to: "home#index"
 
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
